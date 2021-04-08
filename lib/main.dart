0fdf0cdef0d1ff7c6a38fa90b9263b0511e3425b
yam <yamlakyam@gmail.com>
2021-04-08 12:18:59 +0300

    used stateful widget and randomized the dice numbers but the state doesn't seem to change

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -15,20 +17,50 @@ void main() {
   );
 }
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
   @override
   Widget build(BuildContext context) {
+    var random = new Random();
+    var leftDice, rightDice;
+    leftDice = random.nextInt(6);
+    rightDice = random.nextInt(6);
     return Center(
       child: Row(
         children: [
-          Expanded(child: Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Image.asset('images/dice1.png'),
-          ), flex:1,),
-          Expanded(child: Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Image.asset('images/dice1.png'),
-          ), flex: 1,),
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: FlatButton(
+                onPressed: () {
+                  setState(() {
+                  leftDice=5;
+
+                  });
+                },
+                child: Image.asset('images/dice$leftDice.png'),
+                padding: EdgeInsets.symmetric(vertical: 8.0, horizontal: 8.0),
+              ),
+            ),
+            flex: 1,
+          ),
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: FlatButton(
+                onPressed: () {
+                 rightDice=6;
+                },
+                child: Image.asset('images/dice$rightDice.png'),
+                padding: EdgeInsets.symmetric(vertical: 8.0, horizontal: 8.0),
+              ),
+            ),
+            flex: 1,
+          ),
         ],
       ),
     );
