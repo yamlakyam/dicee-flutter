80212d4f76b88e22d78631f2a7e87971dd606e79
yam <yamlakyam@gmail.com>
2021-04-08 10:32:41 +0300

    wrapped the Images with padding

--- lib/main.dart
+++ lib/main.dart
@@ -18,11 +18,19 @@ void main() {
 class DicePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Row(
-      children: [
-        Expanded(child: Image.asset('images/dice1.png'), flex:1),
-        Expanded(child: Image.asset('images/dice1.png'), flex: 1,),
-      ],
+    return Center(
+      child: Row(
+        children: [
+          Expanded(child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Image.asset('images/dice1.png'),
+          ), flex:1,),
+          Expanded(child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Image.asset('images/dice1.png'),
+          ), flex: 1,),
+        ],
+      ),
     );
   }
 }
